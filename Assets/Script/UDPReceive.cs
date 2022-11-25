6d0b511abc42a41f0c58227dfb89a900970e5303
Cath13 <criisj21@student.aau.dk>
2022-11-25 12:19:56 +0100

    prøver at få socket til at virke

--- Assets/Script/UDPReceive.cs
+++ Assets/Script/UDPReceive.cs
@@ -9,7 +9,7 @@ public class UDPReceive : MonoBehaviour
 {
     Thread receiveThread;
     UdpClient client;
-    public int port = 5052;
+    public int port = 5054;
     public bool startReceiving = true;
     public bool printToConsole = false;
     public string data;
