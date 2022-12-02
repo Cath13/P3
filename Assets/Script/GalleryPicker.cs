851661c172a541cccec2abbc79d2b0d9dd9e6128
norhady1 <norhady1@gmail.com>
2022-12-02 13:40:37 +0100

    importere images

---
+++ Assets/Script/GalleryPicker.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalleryPicker : MonoBehaviour
+{
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (NativeGallery.IsMediaPickerBusy())
+					return;
+
+		
+		}
+	}
+
+	public void PickImage(int maxSize)
+	{
+		NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+		{
+			Debug.Log("Image path: " + path);
+			if (path != null)
+			{
+				// Create Texture from selected image
+				Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+				if (texture == null)
+				{
+					Debug.Log("Couldn't load texture from " + path);
+					return;
+				}
+
+				// Assign texture to a temporary quad and destroy it after 5 seconds
+				GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+				quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+				quad.transform.forward = Camera.main.transform.forward;
+				quad.transform.localScale = new Vector3(1.4f, 2.3f, 1f);
+
+				Material material = quad.GetComponent<Renderer>().material;
+				if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+					material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+				material.mainTexture = texture;
+			}
+		});
+
+		Debug.Log("Permission result: " + permission);
+	}
+}
